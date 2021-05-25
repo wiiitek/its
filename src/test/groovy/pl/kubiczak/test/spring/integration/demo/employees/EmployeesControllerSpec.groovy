aeef76b4cbf77ac18c97b6d3acd2d300dcb20232
Wit <kubiczak.jakub@gmail.com>
2021-05-26 01:38:16 +0200

    switch to extend and jupiter SpringExtension

--- src/test/groovy/pl/kubiczak/test/spring/integration/demo/employees/EmployeesControllerSpec.groovy
+++ src/test/groovy/pl/kubiczak/test/spring/integration/demo/employees/EmployeesControllerSpec.groovy
@@ -1,13 +1,13 @@
 package pl.kubiczak.test.spring.integration.demo.employees
 
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import spock.lang.Specification
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebFluxTest(controllers = [
         EmployeesController.class
 ])
