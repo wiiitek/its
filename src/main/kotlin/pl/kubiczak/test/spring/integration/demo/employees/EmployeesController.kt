bc381cbb1f1e6e742a523f6196fef3bf4b8288fd
Wit <kubiczak.jakub@gmail.com>
2022-02-02 14:00:24 +0100

    add contract and endpoint for single employee

--- src/main/kotlin/pl/kubiczak/test/spring/integration/demo/employees/EmployeesController.kt
+++ src/main/kotlin/pl/kubiczak/test/spring/integration/demo/employees/EmployeesController.kt
@@ -2,7 +2,9 @@ package pl.kubiczak.test.spring.integration.demo.employees
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class EmployeesController {
@@ -11,5 +13,11 @@ class EmployeesController {
     fun readAll(): ResponseEntity<List<EmployeeDto>> {
         val list = emptyList<EmployeeDto>()
         return ResponseEntity.ok(list)
+    }
+
+    @GetMapping(path = ["/employees/{uuid}"])
+    fun readAll(@PathVariable uuid: UUID): ResponseEntity<EmployeeDto> {
+        val anEmployee = EmployeeDto(uuid, "John Doe", "john.doe@example.com")
+        return ResponseEntity.ok(anEmployee)
     }
 }
