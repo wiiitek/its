c4ec561f975555a7556478c18ca5a6c9667badbc
Wit <kubiczak.jakub@gmail.com>
2022-02-02 15:13:52 +0100

    update for client and server apps

---
+++ client/src/main/kotlin/pl/kubiczak/test/spring/integration/demo/client/ClientApplication.kt
@@ -0,0 +1,11 @@
+package pl.kubiczak.test.spring.integration.demo.client
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClientApplication>(*args)
+}
