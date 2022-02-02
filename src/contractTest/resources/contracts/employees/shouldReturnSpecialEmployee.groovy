6e463b8d38993b0fe477c2ed167cf63204fe1c3e
Wiiitek <wiiitek@users.noreply.github.com>
2022-02-02 14:14:06 +0100

    add contract and endpoint for single employee (#14)

--- src/contractTest/resources/contracts/employees/shouldReturnSpecialEmployee.groovy
+++ src/contractTest/resources/contracts/employees/shouldReturnSpecialEmployee.groovy
@@ -3,16 +3,16 @@ package contracts.employees
 import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
-    description("Should return empty array")
+    description("Should return special employee")
     request {
         method GET()
-        url '/employees'
+        url '/employees/00000000-0000-0000-0000-000000000000'
     }
     response {
         status OK()
         headers {
             contentType applicationJson()
         }
-        body([])
+        body(uuid: '00000000-0000-0000-0000-000000000000', name: 'John Doe', email: 'john.doe@example.com')
     }
 }
