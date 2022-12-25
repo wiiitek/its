dbbbacdcc4814ee0378f07597d4d1552b65fb339
Wiiitek <wiiitek@example.com>
2022-12-25 13:23:08 +0100

    Upgrade to spring boot 3.0.1

--- server/src/test/groovy/pl/kubiczak/test/spring/integration/demo/employees/EmployeesControllerMockMvcSpec.groovy
+++ server/src/test/groovy/pl/kubiczak/test/spring/integration/demo/employees/EmployeesControllerMockMvcSpec.groovy
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.ApplicationContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
@@ -20,6 +21,9 @@ class EmployeesControllerMockMvcSpec extends Specification {
 
     @MockBean
     EmployeesService employeesService
+
+    @Autowired
+    private ApplicationContext context;
 
     @Autowired
     private MockMvc mockMvc;
