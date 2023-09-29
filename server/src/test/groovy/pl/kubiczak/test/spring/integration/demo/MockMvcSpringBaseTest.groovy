db227892d3b45b0ae6dfce9ea3bd17da48c54f03
wiiitek <wiiitek@users.noreply.github.com>
2023-09-29 08:13:01 +0200

    Use same Spring context for all tests that use @WebMvcTest

--- server/src/test/groovy/pl/kubiczak/test/spring/integration/demo/MockMvcSpringBaseTest.groovy
+++ server/src/test/groovy/pl/kubiczak/test/spring/integration/demo/MockMvcSpringBaseTest.groovy
@@ -1,18 +1,15 @@
 package pl.kubiczak.test.spring.integration.demo
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import pl.kubiczak.test.spring.integration.demo.employees.EmployeesService
 import spock.lang.Specification
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest
-// for spring cloud contract (see build.gradle.kts)
+// Needed also for spring cloud contract (see build.gradle.kts)
 class MockMvcSpringBaseTest extends Specification {
 
     @MockBean
--- server/src/test/groovy/pl/kubiczak/test/spring/integration/demo/employees/EmployeesControllerMockMvcSpec.groovy
+++ server/src/test/groovy/pl/kubiczak/test/spring/integration/demo/employees/EmployeesControllerMockMvcSpec.groovy
@@ -1,25 +1,15 @@
 package pl.kubiczak.test.spring.integration.demo.employees
 
-import org.junit.jupiter.api.extension.ExtendWith
+
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
-import spock.lang.Specification
+import pl.kubiczak.test.spring.integration.demo.MockMvcSpringBaseTest
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = [
-        EmployeesController.class
-])
-class EmployeesControllerMockMvcSpec extends Specification {
-
-    @MockBean
-    EmployeesService employeesService
+class EmployeesControllerMockMvcSpec extends MockMvcSpringBaseTest {
 
     @Autowired
     private MockMvc mockMvc;
