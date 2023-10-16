591f6d4ca902b6ccb55883bdac9b1d5329360100
Wiiitek <wiiitek@users.noreply.github.com>
2023-10-16 21:20:45 +0200

    Add UUID extension (#205)

---
+++ client/src/main/kotlin/pl/kubiczak/test/spring/integration/demo/client/uuid.kt
@@ -0,0 +1,15 @@
+package pl.kubiczak.test.spring.integration.demo.client
+
+import java.util.UUID
+
+private const val maxSupportedInt = 999999999999
+
+fun Long.toUUID(): UUID {
+    check(this in 0..maxSupportedInt) {
+        "Can only convert to UUID integer value from 0 to $maxSupportedInt"
+    }
+    return this.toString().padStart(11, '0')
+        .let { lastPart ->
+            UUID.fromString("00000000-0000-0000-a000-$lastPart")
+        }
+}
