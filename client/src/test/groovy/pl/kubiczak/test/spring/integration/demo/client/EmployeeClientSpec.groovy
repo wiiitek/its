08ea39b7e26b9ca9e75a48aba39d25bf0c6dc1ef
wiiitek <wiiitek@users.noreply.github.com>
2023-10-16 17:29:01 +0200

    Update for contract tests in client

--- client/src/test/groovy/pl/kubiczak/test/spring/integration/demo/client/EmployeeClientSpec.groovy
+++ client/src/test/groovy/pl/kubiczak/test/spring/integration/demo/client/EmployeeClientSpec.groovy
@@ -1,6 +1,7 @@
 package pl.kubiczak.test.spring.integration.demo.client
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 
 class EmployeeClientSpec extends BaseClientApplicationSpec {
 
@@ -12,5 +13,48 @@ class EmployeeClientSpec extends BaseClientApplicationSpec {
         employeeClient != null
         and:
         employeeClient instanceof EmployeeClient
+    }
+
+    def "should receive empty list of employees"() {
+        when:
+        def response = employeeClient.listEmployees().execute()
+
+        then:
+        response.code() == HttpStatus.OK.value()
+        and:
+        response.body() == []
+    }
+
+    def "should receive 404 for non-existent employee"() {
+        given:
+        def nonExistingEmployeeID = UUID.fromString("00000000-0000-0000-0000-000000000000")
+
+        when:
+        def response = employeeClient.findEmployee(nonExistingEmployeeID).execute()
+
+        then:
+        response.code() == HttpStatus.NOT_FOUND.value()
+        and:
+        response.body() == null
+    }
+
+    def "should receive sample employee"() {
+        given:
+        def idIntValue = 1
+        def sampleEmployeeId = UuidKt.toUUID(idIntValue)
+        def expectedEmployeeId = UUID.fromString("00000000-0000-0000-a000-000000000001")
+
+        when:
+        def response = employeeClient.findEmployee(sampleEmployeeId).execute()
+
+        then:
+        response.code() == HttpStatus.OK.value()
+
+        and:
+        with(response.body()) {
+            it.uuid == expectedEmployeeId
+            it.name == "John Doe"
+            it.email == "john.doe@example.com"
+        }
     }
 }
