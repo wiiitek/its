dfd2998e60e96402e3366637d3a9c56c181db76e
wiiitek <wiiitek@users.noreply.github.com>
2023-11-23 21:00:16 +0100

    Upgrade OWASP dependency check plugin

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.20"
     id("groovy")
 
-    id("org.owasp.dependencycheck") version "8.4.3"
+    id("org.owasp.dependencycheck") version "9.0.0"
 }
 
 val javaVersion = JavaVersion.VERSION_17
