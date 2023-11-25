e71661357755ce5259ec04dea21944829678a738
wiiitek <wiiitek@users.noreply.github.com>
2023-11-25 17:59:54 +0100

    Add test-case for invalid response from cats API

--- server/src/test/groovy/pl/kubiczak/test/spring/integration/demo/server/cats/CatFactClientSpec.groovy
+++ server/src/test/groovy/pl/kubiczak/test/spring/integration/demo/server/cats/CatFactClientSpec.groovy
@@ -3,13 +3,17 @@ package pl.kubiczak.test.spring.integration.demo.server.cats
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.codec.DecodingException
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestPropertySource
 import pl.kubiczak.test.spring.integration.demo.server.cats.ports.CatFactClient
 import pl.kubiczak.test.spring.integration.demo.server.config.CatFactConfig
 import spock.lang.Specification
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor
+import static com.github.tomakehurst.wiremock.client.WireMock.get
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor
 
 @ContextConfiguration(classes = [CatFactConfig.class])
 @TestPropertySource(properties = ["apiDomain=http://localhost:8080"])
@@ -30,6 +34,15 @@ class CatFactClientSpec extends Specification {
                         {
                             "fact": "a fact",
                             "length": 6
+                        }
+                        """)
+    }
+
+    private ResponseDefinitionBuilder invalidResponse() {
+        return okJson("""
+                        {
+                            "fact": 12345,
+                            "length": "five"
                         }
                         """)
     }
@@ -63,5 +76,20 @@ class CatFactClientSpec extends Specification {
         actual.fact.length() == expectedLength
         // verifies declared length
         actual.length == expectedLength
+    }
+
+    def "should ... for invalid response"() {
+        given:
+        stubFor(
+                get("/fact").willReturn(invalidResponse())
+        )
+
+        when:
+        catFactClient.nextFact()
+
+        then:
+        def exception = thrown(DecodingException)
+        and:
+        exception.message == "JSON decoding error: Cannot deserialize value of type `long` from String \"five\": not a valid `long` value"
     }
 }
