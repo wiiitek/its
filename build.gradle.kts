f56c7e01b069952ecad5bfea7b0f04115891d59d
wiiitek <wiiitek@users.noreply.github.com>
2024-10-18 20:53:51 +0200

    Move dependency check plugin to parent Gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.21" apply false
+
+    id("org.owasp.dependencycheck") version "10.0.4"
 }
 
 allprojects {
@@ -8,5 +10,16 @@ allprojects {
     }
 }
 
+// http://jeremylong.github.io/DependencyCheck/dependency-check-gradle/configuration.html
+dependencyCheck {
+    formats = listOf("HTML", "JUNIT")
+    failBuildOnCVSS = 3.14f
+    suppressionFile = "owasp-dependency-suppression.xml"
+    // disable .NET assembly scanning
+    analyzers.assemblyEnabled = false
+    nvd.apiKey = System.getenv("NVD_API_KEY")
+    // https://github.com/jeremylong/DependencyCheck/issues/6107#issuecomment-1824010802
+    nvd.delay = 16000
+}
 
 defaultTasks("clean", "build")
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     id("groovy")
 
     id("org.springframework.cloud.contract") version "4.1.4"
-    id("org.owasp.dependencycheck") version "10.0.4"
 }
 
 val javaVersion = JavaVersion.VERSION_17
@@ -19,18 +18,6 @@ group = "pl.kubiczak.test.spring.integration.demo"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = javaVersion
 java.targetCompatibility = javaVersion
-
-// http://jeremylong.github.io/DependencyCheck/dependency-check-gradle/configuration.html
-dependencyCheck {
-    formats = listOf("HTML", "JUNIT")
-    failBuildOnCVSS = 3.14f
-    suppressionFile = "owasp-dependency-suppression.xml"
-    // disable .NET assembly scanning
-    analyzers.assemblyEnabled = false
-    nvd.apiKey = System.getenv("NVD_API_KEY")
-    // https://github.com/jeremylong/DependencyCheck/issues/6107#issuecomment-1824010802
-    nvd.delay = 16000
-}
 
 val vExposed = "0.55.0"
 val vSpock = "2.4-M4-groovy-4.0"
